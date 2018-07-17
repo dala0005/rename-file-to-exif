3353712182f69a87aa504ecf7c29581bb9043307
Daniel Larsson <dan1el_larss0n@protonmail.com>
2018-07-17 09:04:18 +0200

    check filename extension before check exif information. Removed png extension support for now

--- rename-file-to-exif.sh
+++ rename-file-to-exif.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# check if script has argument
+
 if [ -z "$1" ] ; then
    echo "Error: Missing filename to modify"
    exit 0
@@ -7,20 +9,28 @@ fi
 
 filename="$1"
 
-# check if filename already have good format
+# check if filename already have right format
 if [[ $filename =~ ^[0-9]{8}_[0-9]{6} ]] ; then
-   echo "No modifying on filename: $filename - already in good format"
+   echo "No modifying on filename: $filename - already in right format"
    exit 0
 fi
 
-
-path=$(pwd)'/'
-command="identify -verbose $path\"$filename\" | grep \"exif:DateTimeOriginal:\""
 
 #check if file has jpg|JPG extend
 
 filename_len=${#filename}
 filename_extend=${filename:filename_len-3:3}
+
+
+if ! [[ $filename_extend =~ [Jj][Pp][Gg]$ ]] ; then
+  echo "Error: file extension has not [Jj][Pp][Gg]$"
+  echo "The script will now quit"
+  exit 0
+fi
+
+
+path=$(pwd)'/'
+command="identify -verbose $path\"$filename\" | grep \"exif:DateTimeOriginal:\""
 
 year=""
 month=""
@@ -30,11 +40,6 @@ minute=""
 second=""
 filename_new=""
 
-if ! [[ $filename_extend =~ [JjPp][PpNn][Gg]$ ]] ; then
-  echo "Error: file extension has not [Jj][Pp][Gg]$"
-  echo "The script will now quit"
-  exit 0
-fi
 echo "Loading exif information on $path$filename..."
 exif_result=$(eval $command)
 
@@ -56,21 +61,21 @@ if [ -z "$exif_result" ] ; then
 
 else
 
-year=${exif_result:27:4}
-month=${exif_result:32:2}
-day=${exif_result:35:2}
-hour=${exif_result:38:2}
-minute=${exif_result:41:2}
-second=${exif_result:44:2}
-filename_new=$year$month$day\_$hour$minute$second\.jpg
+  year=${exif_result:27:4}
+  month=${exif_result:32:2}
+  day=${exif_result:35:2}
+  hour=${exif_result:38:2}
+  minute=${exif_result:41:2}
+  second=${exif_result:44:2}
+  filename_new=$year$month$day\_$hour$minute$second\.jpg
 
 
-if ! [ "$filename_new" == "$filename" ] ; then
+  if ! [ "$filename_new" == "$filename" ] ; then
     mv "$path$filename" "$path$filename_new"
     echo "The file: $filename has been changed to: $filename_new in path: $path"
     exit 0
-else
-   echo "The filename has already the right format"
+  else
+    echo "The filename has already the right format"
 fi
 
 exit 0
